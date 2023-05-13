8fb38bae56befe12cbb71c5d4eebcfcf4ea6b86d
chosw1002@naver.com <chosw1002@naver.com>
2023-05-13 16:20:14 +0900

    feat: Sender.java의 connectionSetup

--- src/Sender/Sender.java
+++ src/Sender/Sender.java
@@ -1,13 +1,22 @@
 package Sender;
 
+import Packet.Ack;
 import Packet.DatagramBuilder;
 import Packet.Segment;
+import Receiver.Receiver;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Properties;
 
 public class Sender {
-    private static  final Sender instance = new Sender();
+    private static final Sender instance = new Sender();
     private SenderWindow senderWindow = new SenderWindow();
+    private Integer advWindow;
 
     private Timer timer;
+    private Receiver receiver;
+    private boolean isConnected = false;
     private SegmentBuilder segmentBuilder = SegmentBuilder.getInstance();
     private int nextSeqNumber;
 
@@ -22,5 +31,30 @@ public class Sender {
     public void getData(String data) {
         Segment segment = segmentBuilder.makeSegment(data);
         senderWindow.insert(segment);
+    }
+    // TODO: handShaking 과정에서 오고 갈 정보 설정하기
+    public boolean connectionSetup(Receiver receiver) {
+        String resource = "config/RDTP.properties";
+        Properties properties = new Properties();
+        InputStream inputStream = getClass().getClassLoader().getResourceAsStream(resource);
+        try {
+            properties.load(inputStream);
+        } catch (IOException e) {
+            System.out.println("can not open configuration file");
+        }
+        this.advWindow = receiver.handShaking(Long.valueOf(properties.getProperty("sender_init_seq_no"))).getW();
+        this.isConnected = true;
+        return isConnected;
+    }
+
+    public boolean connectionClose() {
+        boolean isConnected =  receiver.close();
+        receiver = null;
+        return isConnected;
+    }
+
+    public void acked(Ack ack) {
+        this.advWindow = ack.getW();
+        // TODO: y에 대한 처리
     }
 }
