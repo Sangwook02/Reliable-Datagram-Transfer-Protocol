2eed5e81167e73adebf57757273acf58246b41da
chosw1002@naver.com <chosw1002@naver.com>
2023-05-21 02:18:13 +0900

    feat: receiver의 window에 segment가 삽입되는 과정 추가

--- src/Receiver/ReceiverBuffer.java
+++ src/Receiver/ReceiverBuffer.java
@@ -1,13 +1,17 @@
 package Receiver;
 
+import Packet.Segment;
+
 import java.io.IOException;
 import java.io.InputStream;
+import java.util.ArrayList;
 import java.util.Properties;
 
 public class ReceiverBuffer {
     private int windowSize;
     private int rcvBase;
     private int lastByteRcvd;
+    private ArrayList<Segment> window;
 
     public ReceiverBuffer() {
         String resource = "config/RDTP.properties";
@@ -21,7 +25,7 @@ public class ReceiverBuffer {
         this.windowSize = Integer.parseInt(properties.getProperty("receiver_window_size"));
         this.rcvBase = 0;
         this.lastByteRcvd = 0;
-
+        this.window = new ArrayList<Segment>(windowSize);
     }
 
     public int getWindowSize() {
@@ -31,5 +35,18 @@ public class ReceiverBuffer {
     public int getRcvBase() {
         return rcvBase;
     }
+
+    public ArrayList<Segment> getWindow() {
+        return window;
+    }
+
+    public void insert(Segment segment) {
+        if (!window.contains(segment)) {
+            window.add(segment);
+        }
+        else {
+            System.out.println("segment is already in window");
+        }
+       }
     // TODO: upper layer에서 data 읽어가는 경우의 작동
 }
