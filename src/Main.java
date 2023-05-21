305f5fda497f0637d2da7c052a6efa83279781cf
chosw1002@naver.com <chosw1002@naver.com>
2023-05-21 12:20:35 +0900

    feat: Control을 통해 실행하도록 수정

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,15 @@
+import java.io.FileNotFoundException;
+
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+    public static void main(String[] args) throws FileNotFoundException, InterruptedException {
+        /*
+        * sequence number의 가용범위는 2^32.
+        * sender의 window size는 1024bytes
+        * receiver의 window size는 512bytes
+        * */
+
+        // TODO: ReceiverUpperApplication을 Thread로 실행하여 read 동작이 계속해서 일어나도록 할 것.
+        Control control = new Control();
+        control.run();
     }
 }
