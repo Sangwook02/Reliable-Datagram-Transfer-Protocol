de611e3b084bb992ffc503f283de53a67d5e2c73
chosw1002@naver.com <chosw1002@naver.com>
2023-05-26 21:57:42 +0900

    refactor: parameter로 들어오는 graphics의 변수명 수정

--- src/CustomCanvas/CustomCanvas.java
+++ src/CustomCanvas/CustomCanvas.java
@@ -38,80 +38,82 @@ public class CustomCanvas extends Canvas {
             drawSegments(graphics);
         } else {
             System.out.println("something went wrong");
-            return;
         }
-
     }
 
-    private void drawWindow(Graphics g) {
+    private void drawWindow(Graphics graphics) {
         int x = 40;
         int y = 150;
         for(WindowElement element:windowElements) {
             if (element.getSequenceNumber() == null) {
                 x += 30;
-                drawNotSentElement(g, element, x, y);
+                drawNotSentElement(graphics, element, x, y);
             } else if (element.getSequenceNumber() > sendBase) { // RED가 아닌 것
                 x += 30;
-                drawWindowElement(g, element, x, y);
+                drawWindowElement(graphics, element, x, y);
             } else if (element.getSequenceNumber() == sendBase) {
-                drawArrow(g, x+25,310);
+                drawArrow(graphics, x+25,310);
                 char a[] = "sendBase".toCharArray();
-                g.drawChars(a, 0 ,a.length, x, 360);
+                graphics.drawChars(a, 0 ,a.length, x, 360);
                 x += 30;
-                drawWindowElement(g, element, x, y);
+                drawWindowElement(graphics, element, x, y);
             } else {
                 x += 30;
-                drawSentAndAckedElement(g, element, x, y);
+                drawSentAndAckedElement(graphics, element, x, y);
             }
         }
     }
-    private void drawSentAndAckedElement(Graphics g, WindowElement element, int x, int y) {
-        g.setColor(Color.RED);
-        g.drawRect(x, y, 20, 150);
-        g.drawString(element.getSequenceNumber().toString(),x+3, y+50);
-        g.drawString(String.valueOf(element.getLength()),x+3, y+100);
+
+    private void drawSentAndAckedElement(Graphics graphics, WindowElement element, int x, int y) {
+        graphics.setColor(Color.RED);
+        graphics.drawRect(x, y, 20, 150);
+        graphics.drawString(element.getSequenceNumber().toString(),x+3, y+50);
+        graphics.drawString(String.valueOf(element.getLength()),x+3, y+100);
     }
-    private void drawNotSentElement(Graphics g, WindowElement element, int x, int y) {
-        g.setColor(Color.GREEN);
-        g.drawRect(x, y, 20, 150);
-        g.drawString(String.valueOf(element.getLength()),x+3, y+100);
+
+    private void drawNotSentElement(Graphics graphics, WindowElement element, int x, int y) {
+        graphics.setColor(Color.GREEN);
+        graphics.drawRect(x, y, 20, 150);
+        graphics.drawString(String.valueOf(element.getLength()),x+3, y+100);
     }
-    private void drawWindowElement(Graphics g, WindowElement element, int x, int y) {
+
+    private void drawWindowElement(Graphics graphics, WindowElement element, int x, int y) {
         if (element.getTimeSent() == null) {
-            g.setColor(Color.GREEN);
-            g.drawRect(x, y, 20, 150);
-            g.drawString(element.getSequenceNumber().toString(),x+3, y+50);
-            g.drawString(String.valueOf(element.getLength()),x+3, y+100);
+            graphics.setColor(Color.GREEN);
+            graphics.drawRect(x, y, 20, 150);
+            graphics.drawString(element.getSequenceNumber().toString(),x+3, y+50);
+            graphics.drawString(String.valueOf(element.getLength()),x+3, y+100);
         }
         else {
-            g.setColor(Color.BLUE);
-            g.drawRect(x, y, 20, 150);
-            g.drawString(element.getSequenceNumber().toString(),x+3, y+50);
-            g.drawString(String.valueOf(element.getLength()),x+3, y+100);
+            graphics.setColor(Color.BLUE);
+            graphics.drawRect(x, y, 20, 150);
+            graphics.drawString(element.getSequenceNumber().toString(),x+3, y+50);
+            graphics.drawString(String.valueOf(element.getLength()),x+3, y+100);
         }
     }
-    private void drawArrow(Graphics g, int x, int y) {
-        g.drawLine(x,y, x-10, y+10);
-        g.drawLine(x,y, x+10, y+10);
-        g.drawLine(x,y, x, y+30);
+
+    private void drawArrow(Graphics graphics, int x, int y) {
+        graphics.drawLine(x,y, x-10, y+10);
+        graphics.drawLine(x,y, x+10, y+10);
+        graphics.drawLine(x,y, x, y+30);
     }
-    private void drawSegments(Graphics g) {
+
+    private void drawSegments(Graphics graphics) {
         int x = 40;
         int y = 150;
         for(Segment segment:segments) {
             if (segment.getSequenceNumber() < rcvBase) { // RED
                 x += 30;
-                // TODO: draw method 추가
-                drawRcvedAndReadSegment(g, segment, x, y);
+                drawRcvedAndReadSegment(graphics, segment, x, y);
             } else if (segment.getSequenceNumber() == rcvBase) {
-                drawArrow(g, x+25,310);
+                drawArrow(graphics, x+25,310);
                 char a[] = "rcvBase".toCharArray();
-                g.drawChars(a, 0 ,a.length, x, 360);
+                graphics.drawChars(a, 0 ,a.length, x, 360);
                 x += 30;
-                // TODO: draw method 추가
+                drawRcvedAndNotReadSegment(graphics, segment, x, y);
             } else if (segment.getSequenceNumber() > rcvBase){ // BLUE
                 x += 30;
-                drawRcvedAndNotReadSegment(g, segment, x, y);
+                drawRcvedAndNotReadSegment(graphics, segment, x, y);
             } else {
                 System.out.println("something went wrong");
                 return;
